7fb9c54fe30aca9a99d8fc4ab209615bb723207e
mingyu.lsm <mingyu.lsm@alibaba-inc.com>
2016-08-03 16:16:36 +0800

    add client

---
+++ client/src/main/java/net/github/gearman/client/NetworkGearmanWorker.java
@@ -0,0 +1,185 @@
+package net.github.gearman.client;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.github.gearman.common.Job;
+import net.github.gearman.common.JobState;
+import net.github.gearman.common.JobStatus;
+import net.github.gearman.common.events.WorkEvent;
+import net.github.gearman.common.interfaces.GearmanFunction;
+import net.github.gearman.common.interfaces.GearmanWorker;
+import net.github.gearman.common.packets.Packet;
+import net.github.gearman.common.packets.request.CanDo;
+import net.github.gearman.common.packets.request.GrabJob;
+import net.github.gearman.common.packets.request.PreSleep;
+import net.github.gearman.common.packets.response.JobAssign;
+import net.github.gearman.common.packets.response.JobAssignUniq;
+import net.github.gearman.common.packets.response.WorkCompleteResponse;
+import net.github.gearman.common.packets.response.WorkDataResponse;
+import net.github.gearman.common.packets.response.WorkStatus;
+import net.github.gearman.common.packets.response.WorkWarningResponse;
+import net.github.gearman.constants.PacketType;
+import net.github.gearman.net.Connection;
+import net.github.gearman.net.ConnectionPool;
+
+public class NetworkGearmanWorker implements GearmanWorker, Runnable {
+
+    private final ConnectionPool               connectionPool;
+    private final Map<String, GearmanFunction> callbacks;
+    private final AtomicBoolean                isActive;
+
+    private static Logger                      LOG = LoggerFactory.getLogger(NetworkGearmanWorker.class);
+    private final Map<Job, Connection>         jobConnectionMap;
+
+    private NetworkGearmanWorker(){
+        this.connectionPool = new ConnectionPool();
+        this.callbacks = new HashMap<>();
+        this.isActive = new AtomicBoolean(true);
+        this.jobConnectionMap = new ConcurrentHashMap<>();
+    }
+
+    private NetworkGearmanWorker(NetworkGearmanWorker other){
+        this.connectionPool = other.connectionPool;
+        this.callbacks = other.callbacks;
+        this.jobConnectionMap = other.jobConnectionMap;
+        this.isActive = new AtomicBoolean(true);
+    }
+
+    @Override
+    public void registerCallback(String method, GearmanFunction function) {
+        callbacks.put(method, function);
+        broadcastAbility(method);
+    }
+
+    @Override
+    public void doWork() {
+        while (isActive.get()) {
+            for (Connection c : connectionPool.getGoodConnectionList()) {
+                LOG.debug("Trying " + c.toString());
+                Job nextJob = null;
+
+                try {
+                    c.sendPacket(new GrabJob());
+                    Packet p = c.getNextPacket();
+                    byte[] result;
+
+                    switch (p.getType()) {
+                        case JOB_ASSIGN:
+                            JobAssign jobAssign = (JobAssign) p;
+                            nextJob = jobAssign.getJob();
+                            break;
+                        case JOB_ASSIGN_UNIQ:
+                            JobAssignUniq jobAssignUniq = (JobAssignUniq) p;
+                            nextJob = jobAssignUniq.getJob();
+                            break;
+                        case NO_JOB:
+                            LOG.info("Worker sending PRE_SLEEP and sleeping for 30 seconds...");
+                            c.sendPacket(new PreSleep());
+                            try {
+                                Packet noop = c.getNextPacket(30 * 1000);
+                                if (noop.getType() != PacketType.NOOP) {
+                                    LOG.error("Received invalid packet. Expected NOOP, received " + noop.getType());
+                                }
+                            } catch (SocketTimeoutException e) {
+                                LOG.warn("Socket timed out waiting for next packet...");
+                            }
+                            break;
+                    }
+
+                    if (nextJob != null) {
+                        jobConnectionMap.put(nextJob, c);
+                        WorkEvent workEvent = new WorkEvent(nextJob, this);
+                        result = callbacks.get(nextJob.getFunctionName()).process(workEvent);
+                        c.sendPacket(new WorkCompleteResponse(nextJob.getJobHandle(), result));
+                    }
+
+                } catch (IOException ioe) {
+                    LOG.error("I/O error: ", ioe);
+                } finally {
+                    if (nextJob != null) jobConnectionMap.remove(nextJob);
+                }
+
+            }
+        }
+        LOG.debug("Worker has been stopped.");
+    }
+
+    @Override
+    public void stopWork() {
+        LOG.debug("Stopping network gearman worker.");
+        isActive.set(false);
+        this.connectionPool.shutdown();
+    }
+
+    @Override
+    public void sendData(Job job, byte[] data) throws IOException {
+        WorkDataResponse workDataResponse = new WorkWarningResponse(job.getJobHandle(), data);
+        Connection c = jobConnectionMap.get(job);
+        c.sendPacket(workDataResponse);
+        LOG.debug("Sent data with " + data.length + " bytes to " + c);
+    }
+
+    @Override
+    public void sendStatus(Job job, int numerator, int denominator) throws IOException {
+        JobStatus jobStatus = new JobStatus(numerator, denominator, JobState.WORKING, job.getJobHandle());
+        Connection c = jobConnectionMap.get(job);
+        c.sendPacket(new WorkStatus(jobStatus));
+        LOG.debug("Sent work status of " + jobStatus + " to " + c);
+
+    }
+
+    @Override
+    public void sendWarning(Job job, byte[] warning) throws IOException {
+        WorkWarningResponse workWarningResponse = new WorkWarningResponse(job.getJobHandle(), warning);
+        Connection c = jobConnectionMap.get(job);
+        c.sendPacket(workWarningResponse);
+        LOG.debug("Sent warning with " + warning.length + " bytes to " + c);
+    }
+
+    private void broadcastAbility(String functionName) {
+        for (Connection c : connectionPool.getGoodConnectionList()) {
+            try {
+                c.sendPacket(new CanDo(functionName));
+            } catch (IOException e) {
+                LOG.error("IO Exception: ", e);
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        // To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public static class Builder {
+
+        private NetworkGearmanWorker worker;
+
+        public Builder(){
+            this.worker = new NetworkGearmanWorker();
+        }
+
+        public NetworkGearmanWorker build() {
+            return new NetworkGearmanWorker(worker);
+        }
+
+        public Builder withConnection(Connection c) {
+            worker.connectionPool.addConnection(c);
+            return this;
+        }
+
+        public Builder withHostPort(String host, int port) {
+            worker.connectionPool.addHostPort(host, port);
+            return this;
+        }
+    }
+
+}
---
+++ client/src/main/java/net/github/gearman/client/NetworkGearmanWorkerPool.java
@@ -0,0 +1,143 @@
+package net.github.gearman.client;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.github.gearman.common.Job;
+import net.github.gearman.common.interfaces.GearmanFunction;
+import net.github.gearman.common.interfaces.GearmanWorker;
+import net.github.gearman.net.Connection;
+
+/**
+ * A multi-threaded network-connected worker pool.
+ */
+public class NetworkGearmanWorkerPool implements GearmanWorker {
+
+    private int                            threadCount;
+    private final List<Connection>         connectionList;
+    private final Set<GearmanWorkerThread> threadSet;
+    private final Set<GearmanWorker>       workerSet;
+
+    private final AtomicBoolean            isActive;
+
+    private static Logger                  LOG = LoggerFactory.getLogger(NetworkGearmanWorkerPool.class);
+
+    private NetworkGearmanWorkerPool(){
+        this.threadCount = 1;
+        this.connectionList = new LinkedList<>();
+        this.threadSet = new HashSet<>();
+        this.workerSet = new HashSet<>();
+        this.isActive = new AtomicBoolean(true);
+    }
+
+    private void initialize() {
+        for (int i = 0; i < threadCount; i++) {
+            NetworkGearmanWorker.Builder workerBuilder = new NetworkGearmanWorker.Builder();
+
+            for (Connection c : connectionList) {
+                workerBuilder.withConnection(new Connection(c));
+            }
+
+            NetworkGearmanWorker worker = workerBuilder.build();
+            threadSet.add(new GearmanWorkerThread(worker));
+            workerSet.add(worker);
+        }
+    }
+
+    @Override
+    public void doWork() {
+        int counter = 0;
+        for (GearmanWorkerThread t : threadSet) {
+            LOG.debug("Starting worker #" + counter++);
+            t.start();
+        }
+
+        for (GearmanWorkerThread t : threadSet) {
+            try {
+                t.join();
+                LOG.debug("Thread " + t.getId() + " joined.");
+            } catch (InterruptedException e) {
+                LOG.error("Error waiting for worker thread: ", e);
+            }
+        }
+
+    }
+
+    @Override
+    public void stopWork() {
+        for (GearmanWorker worker : workerSet) {
+            worker.stopWork();
+        }
+    }
+
+    @Override
+    public void sendData(Job job, byte[] data) throws IOException {
+        // NOOP -- handled by worker
+    }
+
+    @Override
+    public void sendStatus(Job job, int numerator, int denominator) throws IOException {
+        // NOOP -- handled by worker
+    }
+
+    @Override
+    public void sendWarning(Job job, byte[] warning) throws IOException {
+        // NOOP -- handled by worker
+    }
+
+    @Override
+    public void registerCallback(String functionName, GearmanFunction function) {
+        for (GearmanWorkerThread t : threadSet) {
+            t.getWorker().registerCallback(functionName, function);
+        }
+    }
+
+    public static class Builder {
+
+        private NetworkGearmanWorkerPool pool;
+
+        public Builder(){
+            this.pool = new NetworkGearmanWorkerPool();
+        }
+
+        public NetworkGearmanWorkerPool build() {
+            pool.initialize();
+            return pool;
+        }
+
+        public Builder threads(int value) {
+            pool.threadCount = value;
+            return this;
+        }
+
+        public Builder withConnection(Connection c) {
+            pool.connectionList.add(c);
+            return this;
+        }
+    }
+
+    static class GearmanWorkerThread extends Thread {
+
+        private NetworkGearmanWorker worker;
+
+        public GearmanWorkerThread(NetworkGearmanWorker worker){
+            this.worker = worker;
+        }
+
+        public NetworkGearmanWorker getWorker() {
+            return worker;
+        }
+
+        @Override
+        public void run() {
+            worker.doWork();
+        }
+    }
+}
